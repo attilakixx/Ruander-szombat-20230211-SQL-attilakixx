552fd8a2a4a0faf856c52132ebe34c7d415c3b94
Attila Kiss <attilakixx@gmail.com>
2023-02-11 11:52:26 +0100

    added-Krisztina-dog

---
+++ Csuka Krisztina/dog_adoption_tabla.sql
@@ -0,0 +1,36 @@
+CREATE SCHEMA `dog_adoption` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci ;
+
+CREATE TABLE `dog_adoption`.`dog` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `transponder` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `breed` VARCHAR(45) NOT NULL DEFAULT 'mix',
+  `sex` TINYINT(2) NOT NULL COMMENT '1:female 2:male',
+  `birthdate` DATE NULL,
+  `color` VARCHAR(45) NOT NULL,
+  `size` VARCHAR(45) NOT NULL,
+  `status` TINYINT(3) NULL COMMENT '1:adopted, 2:not adopted,3:reserved',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `transponder_UNIQUE` (`transponder` ASC) );
+  
+  
+CREATE TABLE `dog_adoption`.`owner` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `address` VARCHAR(200) NULL,
+  `email` VARCHAR(100) NULL,
+  `phone` VARCHAR(45) NULL,
+  `status` TINYINT(2) NULL COMMENT '1: adopted, 3:reserved',
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `dog_adoption`.`basic_vaccination` (
+  `transponder` INT NOT NULL,
+  `distemper` TINYINT(2) NOT NULL COMMENT '1:yes, 2:no',
+  `hepatitis(CVH)` TINYINT(2) NOT NULL COMMENT '1:yes, 2:no',
+  `leptospirosis` TINYINT(2) NOT NULL COMMENT '1:yes, 2:no',
+  `parvovirosis` TINYINT(2) NOT NULL COMMENT '1:yes, 2:no',
+  `rabies` TINYINT(2) NOT NULL COMMENT '1:yes, 2:no',
+  PRIMARY KEY (`transponder`));
+  
---
+++ Csuka Krisztina/feladatok_dog_adoption.sql
@@ -0,0 +1,26 @@
+-- Készítettem dog_adoption néven egy adatbázist
+-- 3 tábla van benne: - dog
+--                    - owner
+--                    - basic_vaccination
+-- A táblák kapcsolata: `dog.transponder` kapcsolódik a `basic_vaccination.transponder` - hez
+--                      'dog.id' kapcsolódik 'owner.id' - hez
+
+
+
+-- feladatok
+-- 1  Listázd ki azokat a kutyákat, amelyek még örökbefogadhatóak. A nevük legyen "free dog"
+-- 2. Számold össze, hány fekete kutyának van gazgája. 
+-- 3. Listázd ki azokat a barna  és közepes testű kutyákat amiknek hiányzik valamelyik oltásuk.
+-- 4. Számold ki, hogy átlagosan a fiatalabb, vagy az idősebb kutyákat adoptálják.
+-- 5. Listázd ki azokat a kutyákat akik a 2022-es évben születtek és van veszetségi(rabies) és szopornyica(distemper) elleni oltásuk.
+-- 6.
+-- 7.
+-- 8.
+-- 9.
+-- 10.
+-- 11.
+-- 12.
+-- 13.
+-- 14.
+-- 15.
+

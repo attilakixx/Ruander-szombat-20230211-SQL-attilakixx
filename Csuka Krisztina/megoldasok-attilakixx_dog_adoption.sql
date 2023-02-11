e5a52218fc84e879ce4bdc4f1588620c67d57a9a
Attila Kiss <attilakixx@gmail.com>
2023-02-11 12:29:45 +0100

    feladat-3

--- Csuka Krisztina/megoldasok-attilakixx_dog_adoption.sql
+++ Csuka Krisztina/megoldasok-attilakixx_dog_adoption.sql
@@ -4,19 +4,31 @@
 --                    - basic_vaccination
 -- A táblák kapcsolata: `dog.transponder` kapcsolódik a `basic_vaccination.transponder` - hez
 --                      'dog.owner_id' kapcsolódik 'owner.owner_id' - hez
-
-
-
--- feladatok
+ -- feladatok
 -- 1  Listázd ki azokat a kutyákat, amelyek még örökbe fogadhatóak. A nevük legyen "free dog".
 
-SELECT * from dog
+SELECT *
+FROM dog
 WHERE dog.status = 2;
 
 -- 2. Számold össze, hány fekete kutyának van gazdája.
-SELECT count(id) as "Gazdis fekete kutyák" from dog
-where color like "black" and dog.status =1;
+
+SELECT count(id) AS "Gazdis fekete kutyák"
+FROM dog
+WHERE color like "black"
+  AND dog.status =1;
+
 -- 3. Listázd ki, azokat a barna és közepes testű kutyákat, amiknek hiányzik valamelyik oltása.
+
+SELECT *
+FROM dog
+INNER JOIN basic_vaccination ON basic_vaccination.transponder = dog.transponder
+WHERE basic_vaccination.distemper =2
+  OR `hepatitis(CVH)` = 2
+  OR leptospirosis = 2
+  OR parvovirosis = 2
+  OR rabies =2;
+
 -- 4. Számold ki, hogy átlagosan a fiatalabb, vagy az idősebb kutyákat adoptálják.
 -- 5. Listázd ki azokat a kutyákat, akik a 2022-es évben születtek és van veszettségi (rabies) és szopornyica (distemper) elleni oltásuk.
 -- 6.Listázd ki a gazdikat legyen a neve "teljes név" és a hozzájuk tartozó kutyák nevét (adoptált és foglaltak is).
